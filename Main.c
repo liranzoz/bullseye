2e42b438c0ed3a40242d0a283e71fe2c9206c6e9
LiranZ <liranyarinzoz@gmail.com>
2025-11-12 21:14:58 +0200

    12.11

--- Main.c
+++ Main.c
@@ -2,12 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#define ARR_LEN 6
+#define MAX_ROUNDS 10
 
 int make_rand_num();
 
 int main() {
-	int rand_num = make_rand_num();
+	int p, b, rand_num;
+	char sys_rand[ARR_LEN];
+	char user_choice[ARR_LEN];	
 
+	for (int round = 0; round <= MAX_ROUNDS; round++)
+	{
+		printf("Enter 4 number (ex: 1234): ");
+		fgets(user_choice, ARR_LEN, stdin);
+		user_choice[strlen(user_choice) - 1] = '\0';
+
+		rand_num = make_rand_num();
+		for (int i = 0; i < 4; i++) {
+			sys_rand[i] = (rand_num % 10);
+			rand_num /= 10;
+		}
+		sys_rand[strlen(sys_rand) - 1] = '\0';
+		printf("Your number: %s\nSystem number: %s\n", user_choice, sys_rand);
+	}
 	return 0;
 }
 
