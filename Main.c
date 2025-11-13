48250b2145237f76aea5a73de4c3c240e547c9f0
LiranZ <liranyarinzoz@gmail.com>
2025-11-13 15:02:54 +0200

    for nevo

--- Main.c
+++ Main.c
@@ -8,8 +8,9 @@
 int make_rand_num();
 
 int main() {
+	srand(time(0));
 	int p, b, rand_num;
-	char sys_rand[ARR_LEN];
+	char sys_rand [5];
 	char user_choice[ARR_LEN];	
 
 	for (int round = 0; round <= MAX_ROUNDS; round++)
@@ -18,20 +19,19 @@ int main() {
 		fgets(user_choice, ARR_LEN, stdin);
 		user_choice[strlen(user_choice) - 1] = '\0';
 
-		rand_num = make_rand_num();
+		
 		for (int i = 0; i < 4; i++) {
-			sys_rand[i] = (rand_num % 10);
-			rand_num /= 10;
+			rand_num = make_rand_num();
+			sys_rand[i] = rand_num + '0';
 		}
-		sys_rand[strlen(sys_rand) - 1] = '\0';
-		printf("Your number: %s\nSystem number: %s\n", user_choice, sys_rand);
+		sys_rand[4] = '\0';
+		printf("Your number: %s\nSystem number: %s\n\n", user_choice, sys_rand);
 	}
 	return 0;
 }
 
 int make_rand_num()
 {
-	srand(time(0));
 	int rand_num = rand() % 10;
 	return rand_num;
 }
