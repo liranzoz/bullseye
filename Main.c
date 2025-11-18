158b581a9f4287106b8f14dc2e6b875da53d3d57
LiranZ <liranyarinzoz@gmail.com>
2025-11-18 18:47:10 +0200

    test1

--- Main.c
+++ Main.c
@@ -7,6 +7,8 @@
 #define MAX_ROUNDS 10
 
 int make_rand_num();
+int get_p(char arr1[], char arr2[]);
+int get_b(char arr1[], char arr2[]);
 
 int main() {
 	srand(time(0));
@@ -14,28 +16,19 @@ int main() {
 	char sys_rand [5];
 	char user_choice[ARR_LEN];	
 
+	for (int i = 0; i < 4; i++) {
+				rand_num = make_rand_num();
+				sys_rand[i] = rand_num + '0';
+			}
+	sys_rand[4] = '\0';
+
+
 	for (int round = 0; round <= MAX_ROUNDS; round++)
 	{
 		printf("Enter 4 number (ex: 1234): ");
 		fgets(user_choice, ARR_LEN, stdin);
 		user_choice[strlen(user_choice) - 1] = '\0';
 
-		
-		for (int i = 0; i < 4; i++) {
-			rand_num = make_rand_num();
-			sys_rand[i] = rand_num + '0';
-		}
-		
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j; j < i; j++) {
-				if (sys_rand[i] == sys_rand[j]) {
-					sys_rand[j] = make_rand_num() + '0';
-				}
-			}
-		}
-
-		sys_rand[4] = '\0';
 		printf("Your number: %s\nSystem number: %s\n\n", user_choice, sys_rand);
 	}
 	return 0;
@@ -45,4 +38,31 @@ int make_rand_num()
 {
 	int rand_num = rand() % 10;
 	return rand_num;
+}
+
+int get_p(char arr1[], char arr2[])
+{
+	int p;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++) {
+			if (i != j && arr1[i] == arr2[j])
+			{
+				p++;
+			}
+		}
+	}
+	return p;
+}
+
+int get_b(char arr1[], char arr2[])
+{
+	int b;
+	for (int i = 0; i < 4; i++)
+	{
+		if (arr1[i] == arr2[i]) {
+			b++;
+		}
+	}
+	return b;
 }
